f1c3e1577b4725af979f4f19a6ea02fe840b5492
christianratliff20-pixel <christianratliff20@gmail.com>
2026-07-21 23:19:52 -0400

    Create render.py

---
+++ app/pipeline/render.py
@@ -0,0 +1,3 @@
+def render(video_path: str, start_seconds: float, end_seconds: float, output_path: str) -> str:
+    """Stub: not yet implemented. Wire up ffmpeg here."""
+    raise NotImplementedError("Wire up ffmpeg cut + caption burn-in")

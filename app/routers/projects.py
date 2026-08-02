b4922c283658f0aaf12076edc7c200d00e8ecd11
christianratliff20-pixel <christianratliff20@gmail.com>
2026-08-02 13:44:57 -0500

    Update projects.py

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from typing import Optional
-from ..dependencies import get_db, get_current_user
 from ..models import Project, Highlight, User
 from ..tasks import process_video_pipeline, run_detection_pass_two
 import uuid
